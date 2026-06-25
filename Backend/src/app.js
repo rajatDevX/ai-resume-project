ea2f7bc9f74af9f356638d6144286430158694f4
rajat <aneetajain205001@gmail.com>
2026-06-25 13:35:36 +0530

    second revised version

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,6 +9,7 @@ app.use(cookieParser());
 
 const allowedOrigins = [
   "http://localhost:5173",
+  "https://ai-resume-project-t083.onrender.com", // ← hardcode kar do
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
