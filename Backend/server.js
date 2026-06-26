2ce09042b860aaa5efc381c079b6e149cbea083b
rajat <aneetajain205001@gmail.com>
2026-06-26 14:05:40 +0530

    26 june second

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,14 @@
-require("dotenv").config()
-const app = require("./src/app")
-const connectToDB = require("./src/config/database")
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-connectToDB()
+const app = require("./src/app");
+const connectToDB = require("./src/config/database");
 
+connectToDB();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
